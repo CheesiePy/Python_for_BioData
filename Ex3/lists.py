01304f6137666df8309691fd82c4705bd4ea7497
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2023-04-11 14:47:46 +0300

    update 11/04/23

---
+++ Ex3/lists.py
@@ -0,0 +1,12 @@
+my_list = []
+for i in range(15):
+    my_list.append(i)
+
+print(my_list[-1])
+
+"""
+ https://www.w3schools.com/python/python_operators.asp 
+
+ use w3 to learn all about python basic syntax 
+
+"""
